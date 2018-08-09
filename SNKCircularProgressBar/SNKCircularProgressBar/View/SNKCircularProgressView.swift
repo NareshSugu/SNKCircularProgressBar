ac5a29a76d39be428353c4b36a9cc2746f6763eb
Sugu <kumar.sugu.naresh@gmail.com>
2018-08-09 13:06:43 +0530

    [iOS][initmethod] - added a constructor which will take radius, and thikness of layers as parameter

--- SNKCircularProgressBar/SNKCircularProgressBar/View/SNKCircularProgressView.swift
+++ SNKCircularProgressBar/SNKCircularProgressBar/View/SNKCircularProgressView.swift
@@ -13,6 +13,9 @@ class SNKCircularProgressView: UIView {
     var bgPath: UIBezierPath!
     var shapeLayer: CAShapeLayer!       // Background layer
     var progressLayer: CAShapeLayer!    // Foreground layer
+    var radius: CGFloat!
+    var thiknessShapeLayer: CGFloat!
+    var thiknessProgressLayer: CGFloat!
 
     var progress: Float = 0 {
         willSet(newValue)
@@ -22,12 +25,17 @@ class SNKCircularProgressView: UIView {
     }
     override init(frame: CGRect) {
         super.init(frame: frame)
-        bgPath = UIBezierPath()
-        self.initCircularProgressBar()
     }
     
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    public init(frame: CGRect, radius: CGFloat, shapeLayerThikness: CGFloat, progressLayerThikness: CGFloat) {
+        super.init(frame: frame)
+        self.radius = radius
+        self.thiknessShapeLayer = shapeLayerThikness
+        self.thiknessProgressLayer = progressLayerThikness
         bgPath = UIBezierPath()
         self.initCircularProgressBar()
     }
@@ -39,7 +47,7 @@ class SNKCircularProgressView: UIView {
         let y = self.frame.height/2
         let center = CGPoint(x: x, y: y)
         print(x,y,center)
-        bgPath.addArc(withCenter: center, radius: x/CGFloat(2), startAngle: CGFloat(0), endAngle: CGFloat(6.28), clockwise: true)
+        bgPath.addArc(withCenter: center, radius: self.radius, startAngle: CGFloat(0), endAngle: CGFloat(6.28), clockwise: true)
         bgPath.close()
     }
     
@@ -47,7 +55,7 @@ class SNKCircularProgressView: UIView {
     {
         shapeLayer = CAShapeLayer()
         shapeLayer.path = bgPath.cgPath
-        shapeLayer.lineWidth = 15
+        shapeLayer.lineWidth = self.thiknessShapeLayer
         shapeLayer.fillColor = nil
         shapeLayer.strokeColor = UIColor.lightGray.cgColor
         
@@ -58,7 +66,7 @@ class SNKCircularProgressView: UIView {
         progressLayer = CAShapeLayer()
         progressLayer.path = bgPath.cgPath
         progressLayer.lineCap = kCALineCapRound
-        progressLayer.lineWidth = 15
+        progressLayer.lineWidth = self.thiknessProgressLayer
         progressLayer.fillColor = nil
         progressLayer.strokeColor = UIColor.red.cgColor
         progressLayer.strokeEnd = 0.0
--- SNKCircularProgressBar/SNKCircularProgressBar/ViewController.swift
+++ SNKCircularProgressBar/SNKCircularProgressBar/ViewController.swift
@@ -15,9 +15,13 @@ class ViewController: UIViewController {
     var progressCounter:Float = 0
     let duration:Float = 10.0
     var progressIncrement:Float = 0
-
+    var circularProgressView: SNKCircularProgressView! = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        circularProgressView = SNKCircularProgressView (frame: circularProgressBarView.frame, radius: 100, shapeLayerThikness: 5, progressLayerThikness: 20)
+        self.view.addSubview(circularProgressView)
+
         progressIncrement = 1.0/duration
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.showProgress), userInfo: nil, repeats: true)
     }
@@ -25,7 +29,7 @@ class ViewController: UIViewController {
     @objc func showProgress()
     {
         if(progressCounter > 1.0){timer.invalidate()}
-        circularProgressBarView.progress = progressCounter
+        circularProgressView.progress = progressCounter
         progressCounter = progressCounter + progressIncrement
     }
 
